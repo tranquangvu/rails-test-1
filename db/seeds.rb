7b1630e82c2ca404df2ee82a359fa04859698148
tranquangvu <vutran.513@gmail.com>
2016-06-10 15:03:50 +0700

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,32 @@
 contents = [
   %(
-    A child asked his father, "How were people born?"
-    So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."
-    The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
-    The child ran back to his father and said, "You lied to me!"
-    His father replied, "No, your mom was talking about her side of the family."
+    <p>A child asked his father, "How were people born?"</p>
+    <p>So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."</p>
+    <p>The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."</p>
+    <p>The child ran back to his father and said, "You lied to me!"</p>
+    <p>His father replied, "No, your mom was talking about her side of the family."</p>
   ),
   %(
-    Teacher: "Kids,what does the chicken give you?"
-    Student: "Meat!"
-    Teacher: "Very good! Now what does the pig give you?"
-    Student: "Bacon!"
-    Teacher: "Great! And what does the fat cow give you?"
-    Student: "Homework!"
+    <p>Teacher: "Kids,what does the chicken give you?"</p>
+    <p>Student: "Meat!"</p>
+    <p>Teacher: "Very good! Now what does the pig give you?"</p>
+    <p>Student: "Bacon!"</p>
+    <p>Teacher: "Great! And what does the fat cow give you?"</p>
+    <p>Student: "Homework!"</p>
   ),
   %(
-    The teacher asked Jimmy, "Why is your cat at school today Jimmy?"
-    Jimmy replied crying, "Because I heard my daddy tell my mommy, 'I am going to eat that pussy once Jimmy leaves for school today!'"
+    <p>The teacher asked Jimmy, "Why is your cat at school today Jimmy?"</p>
+    <p>Jimmy replied crying, "Because I heard my daddy tell my mommy, 'I am going to eat that pussy once Jimmy leaves for school today!'"</p>
   ),
   %(
-    A housewife, an accountant and a lawyer were asked "How much is 2+2?"
-    The housewife replies: "Four!".
-    The accountant says: "I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time."
-    The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?"
+    <p>A housewife, an accountant and a lawyer were asked "How much is 2+2?"</p>
+    <p>The housewife replies: "Four!".</p>
+    <p>The accountant says: "I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time."</p>
+    <p>The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?"</p>
   )
 ]
+
+Joke.delete_all
 
 contents.each do |c|
   Joke.create(content: c)
