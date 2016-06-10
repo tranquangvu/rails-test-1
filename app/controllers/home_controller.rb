07fa2e22076552b609cf1cdc9b7ff898abe4d8ea
tranquangvu <vutran.513@gmail.com>
2016-06-10 17:08:46 +0700

    fix tab length

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def index
-		@joke = current_user.next_joke
-	end
+  def index
+    @joke = current_user.next_joke
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: { thumb: "64x64#" },
-                              default_url: "/images/:style/default_avatar.png"
+  has_attached_file :avatar, styles: { thumb: "64x64#" }, default_url: "/images/:style/default_avatar.png"
 
-  validates_attachment_content_type :avatar,    content_type: /\Aimage\/.*\Z/
-  validates :username,                          presence: true,
-                                                length: { minimum: 3, maximum: 16 }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates :username, presence: true, length: { minimum: 3, maximum: 16 }
 
   def read_jokes
     Joke.joins(:votes).where( votes: { user_id: id } )
   end
 
-    def available_jokes
-        Joke.all - read_jokes
-    end
+  def available_jokes
+    Joke.all - read_jokes
+  end
 
-    def next_joke
-        available_jokes.first
-    end
+  def next_joke
+    available_jokes.first
+  end
 end
